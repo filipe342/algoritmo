0a066565742d14f36bfd7f24e3d79bb42a694f9c
Filipe <filipeenzo2020@gmail.com>
2019-12-02 16:02:23 -0300

    alteracao

---
+++ AlgoritmoComEspaçamentoEntreoIndexDoArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AlgoritmoComEspaçamentoEntreoIndexDoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 5, 4, 3, 2, 1 };
+            int inde = 0;
+            string espac = PegarEspacos(num.Length);
+
+            foreach (int nu in num)
+            {
+
+                Console.WriteLine(espac + num[inde]);
+                inde = inde + 1;
+                espac = PegarEspacos(num.Length - inde);
+
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string PegarEspacos(int quantidadeDeEspacos)
+        {
+            string totalEspacos = "";
+
+            for (int i = 0; i < quantidadeDeEspacos; i++)
+            {
+
+                totalEspacos = totalEspacos + "   ";
+
+            }
+
+            return totalEspacos;
+        }
+    }
+}
